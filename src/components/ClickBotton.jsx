61720987bcfdaff7f7088b14a6b44e0c55ff7a83
Dragoș Străinu <str.dr4605@gmail.com>
2023-02-27 20:06:31 +0200

    add ClickBotton, use it in RedBorder

---
+++ src/components/ClickBotton.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export default class ClickButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { clicked: false };
+    }
+
+    render() {
+        const { hover, afterClickText, children } = this.props;
+
+        if (this.state.clicked) {
+            return (
+                <h2>{afterClickText || "You clicked and liked"}</h2>
+            );
+        }
+
+        let buttonStyle = "bg-transparent text-blue-700 font-semibold py-2 px-4 border border-blue-500 rounded";
+        const hoverStyle = "hover:bg-blue-500 hover:text-white hover:border-transparent"
+
+        if (hover) {
+            buttonStyle = `${buttonStyle} ${hoverStyle}`;
+        }
+
+        return (
+            <button
+                className={buttonStyle}
+                onClick={() => this.setState({ clicked: true })}
+            >
+                {children}
+            </button>
+        );
+    }
+}
--- src/components/RedBorder.jsx
+++ src/components/RedBorder.jsx
@@ -1,7 +1,12 @@
+import ClickButton from "./ClickBotton";
+
 export default function RedBorder({ children }) {
     return (
         <div className="border border-2 border-red-400 p-2">
             Red border container:
+            <ClickButton hover afterClickText="You clicked me">
+                Click me
+            </ClickButton>
             <br></br>
             {children}
         </div>
